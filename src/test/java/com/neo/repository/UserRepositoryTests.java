988fd9e6cf0213cc8772d473c3864ac94964b2ae
Guannian Zeng <vgnzeng@gmail.com>
2018-08-29 10:02:31 -0700

    fiexed test code that returns size of Array instead of Array of objects

--- src/test/java/com/neo/repository/UserRepositoryTests.java
+++ src/test/java/com/neo/repository/UserRepositoryTests.java
@@ -32,7 +32,7 @@ public class UserRepositoryTests {
 		userRepository.save(new User("bb", "bb123456","bb@126.com", "bb",  formattedDate));
 		userRepository.save(new User("cc", "cc123456","cc@126.com", "cc",  formattedDate));
 
-		Assert.assertEquals(3, userRepository.findAll());
+		Assert.assertEquals(3, userRepository.findAll().size());
 		Assert.assertEquals("bb", userRepository.findByUserNameOrEmail("bb", "bb@126.com").getNickName());
 //		userRepository.delete(userRepository.findByUserName("aa"));
 	}
